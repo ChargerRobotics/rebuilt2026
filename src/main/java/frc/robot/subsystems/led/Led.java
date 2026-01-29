b5ebd487e540991ee8f4f96601ff31fa19413cdf
Zachary Su <zach.su158@gmail.com>
2026-01-28 19:16:23 -0700

    updated Led to update and process logged inputs

--- src/main/java/frc/robot/subsystems/led/Led.java
+++ src/main/java/frc/robot/subsystems/led/Led.java
@@ -1,12 +1,21 @@
 package frc.robot.subsystems.led;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Led extends SubsystemBase {
   private final LedControlIO io;
+  private final LedControlIOInputsAutoLogged inputs = new LedControlIOInputsAutoLogged();
 
   public Led(LedControlIO io) {
     this.io = io;
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Led", inputs);
   }
 
   public void lightClimbing() {
