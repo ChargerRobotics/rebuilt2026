3364bd11bf307fbf889b2c2713d862564db808e6
DatAsianBoi123 <zach.su158@gmail.com>
2026-02-09 16:11:21 -0700

    zeroed rotations

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -29,10 +29,10 @@ public class DriveConstants {
       };
 
   // Zeroed rotation values for each module, see setup instructions
-  public static final Rotation2d frontLeftZeroRotation = new Rotation2d(0.0);
-  public static final Rotation2d frontRightZeroRotation = new Rotation2d(0.0);
-  public static final Rotation2d backLeftZeroRotation = new Rotation2d(0.0);
-  public static final Rotation2d backRightZeroRotation = new Rotation2d(0.0);
+  public static final Rotation2d frontLeftZeroRotation = new Rotation2d(2.66357);
+  public static final Rotation2d frontRightZeroRotation = new Rotation2d(0.882082);
+  public static final Rotation2d backLeftZeroRotation = new Rotation2d(-0.657142);
+  public static final Rotation2d backRightZeroRotation = new Rotation2d(1.731864);
 
   // Device CAN IDs
   public static final int pigeonCanId = 13;
