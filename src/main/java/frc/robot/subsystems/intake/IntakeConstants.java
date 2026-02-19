7849ae5296966f4d1e07e3e8c123fffab34abcb2
Zachary Su <zach.su158@gmail.com>
2026-02-18 17:53:45 -0700

    made constants final

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -7,34 +7,34 @@ public class IntakeConstants {
   public static final double rollerReduction = 4 / 1;
 
   public static final DCMotor deployGearbox = DCMotor.getNEO(1);
-  public static double deployReduction = 25 / 1;
-  public static double deployEncoderPositionFactor = 2 * Math.PI / deployReduction;
-  public static double deployEncoderVelocityFactor = (2 * Math.PI) / 60.0 / deployReduction;
+  public static final double deployReduction = 25 / 1;
+  public static final double deployEncoderPositionFactor = 2 * Math.PI / deployReduction;
+  public static final double deployEncoderVelocityFactor = (2 * Math.PI) / 60.0 / deployReduction;
 
-  public static int rollerCanId = 20;
-  public static int deployCanId = 21;
+  public static final int rollerCanId = 20;
+  public static final int deployCanId = 21;
 
-  public static int rollerCurrentLimit = 20;
-  public static double rollerMaxAcceleration = 20000;
-  public static double rollerAllowedProfileError = 1000;
+  public static final int rollerCurrentLimit = 20;
+  public static final double rollerMaxAcceleration = 20000;
+  public static final double rollerAllowedProfileError = 1000;
 
-  public static int deployCurrentLimit = 20;
-  public static double deployCruiseVelocity = 0.5 * Math.PI;
-  public static double deployMaxAcceleration = Math.PI;
-  public static double deployAllowedProfileError = 0.2 * Math.PI;
+  public static final int deployCurrentLimit = 20;
+  public static final double deployCruiseVelocity = 0.5 * Math.PI;
+  public static final double deployMaxAcceleration = Math.PI;
+  public static final double deployAllowedProfileError = 0.2 * Math.PI;
 
-  public static double deployKp = 0;
-  public static double deployKd = 0;
-  public static double deployKg = 0;
-  public static double deployKs = 0;
-  public static double deployKv = 0;
-  public static double deployKa = 0;
+  public static final double deployKp = 0;
+  public static final double deployKd = 0;
+  public static final double deployKg = 0;
+  public static final double deployKs = 0;
+  public static final double deployKv = 0;
+  public static final double deployKa = 0;
 
-  public static double deploySimKp = 5;
-  public static double deploySimKd = 0;
-  public static double deploySimKg = 0;
-  public static double deploySimKs = 0;
-  public static double deploySimKv = 0;
-  public static double deploySimKa = 0;
+  public static final double deploySimKp = 5;
+  public static final double deploySimKd = 0;
+  public static final double deploySimKg = 0;
+  public static final double deploySimKs = 0;
+  public static final double deploySimKv = 0;
+  public static final double deploySimKa = 0;
 }
 
